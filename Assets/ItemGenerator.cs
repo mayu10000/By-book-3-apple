3dc815e34f6a24bb01232be98104b11540a14f62
mayu <trachan15-wan4@nifty.com>
2018-01-05 18:25:46 +0900

    applePreFab　同じProject　の　2回目送りの　test　です

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+// using System.Collections.Generic;
+
+public class ItemGenerator : MonoBehaviour {
+
+
+    public GameObject applePrefab;
+    public GameObject bombPrefab;
+    float span = 1.0f;
+    float delta = 0;
+
+
+
+    // Use this for initialization
+  //  void Start () {
+		
+ // 	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            Instantiate(applePrefab);
+
+           }
+            
+        }
+    }
