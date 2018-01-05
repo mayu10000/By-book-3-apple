bc48a6487f25cc5e67817fc1178d7a808bb69ac3
mayu <trachan15-wan4@nifty.com>
2018-01-05 14:25:18 +0900

    リンゴが床を抜けます　音がなりません　apple　bamb　の表示が出なくなりました

---
+++ Assets/BasketController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+//  using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour {
+
+
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud ;
+
+    void start()
+    {
+        this.aud = GetComponent<AudioSource>();
+
+    }
+
+    
+
+    void OnTriggerEnter(Collider other)  {
+
+
+        if (other.gameObject.tag == "Apple")
+        {
+            this.aud.PlayOneShot(this.appleSE);
+        }
+        else
+        {
+            this.aud.PlayOneShot(this.bombSE);
+        }
+
+
+
+
+        if (other.gameObject.tag == "Apple")
+        {
+            Debug.Log("Tag = Apple");
+        }
+        else
+        {
+            Debug.Log("Tag = Bomb");
+        }
+
+        
+        //	Debug.Log("キャッチ! ");
+        //	Destroy(other.gameObject);
+    }
+
+
+// Use this for initialization
+  //  void Start () {
+		
+  //	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+            
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0.0f, z);
+
+            }
+        }
+    }
+}
+
+
+
+		
+	
+
