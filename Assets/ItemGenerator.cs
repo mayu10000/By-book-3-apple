ee3c929a81e7b78c58310605be5e648b74ef601b
mayu <trachan15-wan4@nifty.com>
2018-01-07 20:53:44 +0900

    apple　や　bomb　 を　catch  したときの、　apple　などの表示が出なくなりました

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -10,24 +10,51 @@ public class ItemGenerator : MonoBehaviour {
     float span = 1.0f;
     float delta = 0;
 
+    int ratio = 2;
+
 
 
     // Use this for initialization
-  //  void Start () {
-		
- // 	}
-	
-	// Update is called once per frame
-	void Update () {
+    //  void Start () {
+
+    // 	}
+
+    // Update is called once per frame
+    void Update () {
 
 
         this.delta += Time.deltaTime;
         if (this.delta > this.span)
         {
             this.delta = 0;
-            Instantiate(applePrefab);
 
-           }
+
+            //   GameObject item = Instantiate(applePrefab) as GameObject;
+
+
+            GameObject item;
+            int dice = Random.Range(1, 11);
+            if (dice <= this.ratio)
+            {
+                item = Instantiate(bombPrefab) as GameObject;
+            }
+            else
+            {
+                item = Instantiate(applePrefab) as GameObject;
+            }
+
+
+
+
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+
+            item.transform.position = new Vector3(x, 4, z);
+
+
+
             
+               }
+ 
+            }
         }
-    }
