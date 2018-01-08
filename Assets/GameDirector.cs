a2a60eb8f3823d3d6eaa136263c90c36c3bb188d
mayu <trachan15-wan4@nifty.com>
2018-01-08 18:59:40 +0900

    表示　apple　bomb　が出ない　以外は、順調です

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+//  using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UIを使うときは忘れないように注意！
+
+
+
+public class GameDirector : MonoBehaviour {
+
+    // Use this for initialization
+
+
+ GameObject timerText ;
+
+float time = 60.0f ;
+
+
+	void Start() {
+
+        this.timerText = GameObject.Find("Time");
+
+       }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        this.time -= Time.deltaTime;
+        this.timerText.GetComponent<Text>().text =
+            this.time.ToString("F1");
+
+        
+    }
+}
